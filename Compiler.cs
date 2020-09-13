fa90d064551ac4ff2945373f02ab1b6fc9559823
Jenni <jfelton@csu.fullerton.edu>
2020-09-13 15:33:46 -0700

    Input choice, Reading in from file completed

---
+++ Compiler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CompilerProject {
+    class Program {
+        static void Main(string[] args) {
+
+            // I don't know if this is really the right way to start off my program
+            // But it currently works starting it this way
+            var inputToCompile = new Inputs();
+
+            inputToCompile.ChooseInputFromFileOrUser();
+            
+            
+
+
+
+
+            //TODO: Write a function that reads in a file 
+
+            //TODO: Write a function that asks for user input to compile
+
+            //TODO: FSM
+
+
+
+          
+
+
+            
+        }
+    }
+}
---
+++ Inputs.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CompilerProject {
+    class Inputs : Program {
+
+        string userChoice;
+        string fileToRead;
+       // string[] lines;
+        string line;
+
+        // TODO: remove unneeded comments when done with Inputs
+
+        // This works
+        public void ChooseInputFromFileOrUser() {
+            Console.WriteLine("Compile from File or User Input?");
+            Console.WriteLine("Choose File or User: ");
+            userChoice = Console.ReadLine();
+
+            try {
+                if (userChoice == "File" || userChoice == "file") {
+                    UserEnterFileName();
+                } 
+                if (userChoice == "User" || userChoice == "user") {
+
+                    // TODO: write function to get user input
+                    Console.WriteLine("User selected");
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("Wrong Selection");
+            }
+        }
+
+        // This works
+        public void UserEnterFileName() {
+
+            Console.WriteLine("Enter the file location");
+            fileToRead = Console.ReadLine();
+           // Console.WriteLine(fileToRead);
+            ReadInFile(@fileToRead);
+        }
+
+        //This works as it is
+        public void ReadInFile(string fileName) {
+
+            try {
+                using (var lines = new StreamReader(fileName)) {
+                    Console.WriteLine(lines.ReadToEnd());
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("The File could not be read: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void FormatIntoPiecs() {
+            // TODO: make this function format both the user input and the file input into it's parts to be tokenized
+        }
+
+    }
+}
