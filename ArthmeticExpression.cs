f125b3e94dba2a05cdcc2b8af973e521828042a6
Jenni <justlittleoldmejenn@gmail.com>
2020-11-07 14:09:41 -0800

    Added classes for rules and test code file

---
+++ ArthmeticExpression.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JenPile {
+    class ArthmeticExpression {
+
+        // <Expression> -> <Expression> + <Term>
+        // <Expression> -> <Expression> - <Term>
+        // <Expression> -> <Term>
+
+        // <Factor> -> (Expression)
+        // <Factor> -> ID
+        // <Factor> -> Num
+
+        // ID -> id
+
+
+
+
+
+
+
+    }
+}
---
+++ DeclarativeStatement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JenPile {
+    class DeclarativeStatement {
+        // <Statement> -> <Declarative>
+        // <Declarative> -> <Type> <ID> 
+        // <Type> -> bool
+        // <Type> -> float
+        // <Type> -> int
+        // rule for semi colon at the end (optional)
+
+    }
+}
