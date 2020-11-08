d80a71c9967e2417f52e508dcde8a53927c7e12e
Jenni <justlittleoldmejenn@gmail.com>
2020-11-07 19:51:58 -0800

    Fixed a rule definition

--- ArthmeticExpression.cs
+++ ArthmeticExpression.cs
@@ -9,6 +9,10 @@ namespace JenPile {
         // <Expression> -> <Expression> - <Term>
         // <Expression> -> <Term>
 
+        // <Term> -> <Term> * <Factor>
+        // <Term> -> <Term> / <Factor> 
+        // <Term> -> <Factor> 
+
         // <Factor> -> (Expression)
         // <Factor> -> ID
         // <Factor> -> Num
