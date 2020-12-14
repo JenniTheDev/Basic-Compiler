be007e1b65ad43c440e5a1eb382773be26260578
Jenni <justlittleoldmejenn@gmail.com>
2020-12-13 16:12:34 -0800

    Created Symbol Table

---
+++ SymbolTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JenPile {
+    internal class SymbolTable {
+        private const int MEMORY_LOCATION = 5000;
+        private Token tokenToAdd;
+
+        public Dictionary<Token, int> IdTable = new Dictionary<Token, int>();
+
+        public void addToTable(Token token) {
+            try {
+                IdTable.Add(tokenToAdd, MEMORY_LOCATION);
+            } catch (ArgumentException) {
+                Console.WriteLine("An identifier with *this* already exists");
+            }
+        }
+    }
+}
